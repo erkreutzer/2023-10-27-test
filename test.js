9546c0064a3cf9a88653dc3f883588187177cf1d
Erich Kreutzer <erich.kreutzer@verkada.com>
2024-05-02 10:37:48 -0400

    Test out directory creation + multiple secrets

--- test.js
+++ test.js
@@ -1,19 +1,24 @@
 const fs = require('fs');
-const file = 'account.json';
-if (!fs.existsSync(file)) {
-  console.log('missing file');
-  return;
+
+function testFile(file) {
+  console.log('testFile', file)
+  if (!fs.existsSync(file)) {
+    console.log('missing file');
+    return;
+  }
+
+  const resp = fs.readFileSync(file, 'utf8');
+
+  console.log('contents:', resp);
+
+  try {
+    const obj = JSON.parse(resp);
+    console.log('username', obj.username);
+    console.log('password', obj.password);
+  } catch (e) {
+    console.log('not able to parse')
+  }
 }
 
-const resp = fs.readFileSync(file, 'utf8');
-
-console.log('contents:', resp);
-// get string respresentation of file at account.json
-
-try {
-  const obj = JSON.parse(resp);
-  console.log('username', obj.username);
-  console.log('password', obj.password);
-} catch (e) {
-  console.log('not able to parse')
-}
+testFile('tools/playwright/accounts/account.json');
+testFile('tools/playwright/accounts/account2.json');
